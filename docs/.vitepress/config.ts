9d0d73a622b85e559a01cf59ac08c00c28c21380
Liu-wei-tao <2467802439@qq.com>
2023-05-31 21:26:55 +0800

    文件

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,91 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'xiaoliu',
+  //左边界面
+  themeConfig: {
+    siteTitle: "小刘,欢迎回家！",
+    logo: "https://cdn.staticaly.com/gh/Liu-wei-tao/myimg@master/文档网站所需要的图片/logo1.4f9rt58nd5s0.webp",
+    
+    
+    nav: [
+      { text: "vitepress脚本一键部署", link: "../md/初始化配置vitepress.md" },
+      { text: "免费gpt账号", link: "../md/user.md" },
+      { text: "技术篇文档", link: "../md/1.阶段回顾与综合架构准备.md" },
+      {
+        text: "Drop Menu",
+        items: [
+          {
+            text: "Item A",
+            items: [
+              { text: "Item A1", link: "/item-A1" },
+              { text: "Item A2", link: "/item-A2" },
+            ],
+          },
+          {
+            text: "Item B",
+            items: [
+              { text: "Item B1", link: "/item-B1" },
+              { text: "Item B2", link: "/item-B2" },
+            ],
+          },
+        ],
+      },
+    ],
+    // 配置社交链接socialLinks
+    socialLinks: [
+      { icon: "github", link: "https://github.com/Liu-wei-tao" },
+      { icon: "twitter", link: "..." },
+      // 您还可以通过传递 SVG 字符串来添加自定义图标：
+      {
+        icon: {
+          svg: '<svg role="img" viewBox="0 0 24 24" xmlns="SVG namespace"><title>Dribbble</title><path d="M12...6.38z"/></svg>',
+        },
+        link: "...",
+      },
+    ],
+
+    //侧边栏
+    sidebar: [
+      {
+        text: "阶段回顾",
+        items: [
+          {
+            text: "阶段回顾与综合架构准备",
+            link: "/md/1.阶段回顾与综合架构准备.md"
+          },
+          {
+            text: "docker快速搭建一个静态网站",
+            link: "/md/2.docker快速搭建一个静态网站.md",
+          },
+        ],
+      },
+      {
+        text: "vitepress脚本一键部署",
+        items: [
+          {
+            text: "初始化配置vitpress",
+            link: "../md/初始化配置vitepress.md",
+          },
+          {
+            text: "用dockerfile进行云部署",
+            link: "/md/docker.md",
+          },
+          
+        ],
+      },
+
+      {
+        text: "免费ai项目",
+        items: [
+          {
+            text: "免费账号",
+            link: "../md/免费账号.md",
+          },
+          
+          
+        ],
+      },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,53 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "小刘，欢迎回家！"
+  text: "技术分享，共同进步！"
+  tagline: 有需要补充的v，kj8888r
+  title: 小刘
+  image:
+    src: https://cdn.staticaly.com/gh/Liu-wei-tao/myimg@master/文档网站所需要的图片/AI1.678qw7whgpg0.webp
+    alt: 
+  actions:
+    - theme: brand
+      text: 技术篇文档
+      link: /md/1.阶段回顾与综合架构准备
+    - theme: alt
+      text: ai项目
+      link: /md/user
+
+features:
+  - icon: ⚡️
+    title: chatgpt网址，记得开科学
+    details: ai.com
+  
+  - icon: 🖖
+    title: 网页版ai，无需科学上网，访问密码abc，api自取
+    details: https://chatgpt.pyliu.top/#/
+
+  
+  - icon: 🛠️
+    title: 接码网站，开科学
+    details: https://sms-activate.org/feedbackTest
+---
+<!-- 🤳
+
+
+
+## Q音(仿抖音)
+
+移动端体验地址：https://snowcyans.gitee.io/qqshortvideo/
+
+🛒
+
+## Q淘
+
+移动端体验地址：https://snowcyans.gitee.io/qamoy/
+
+🎧
+
+## QQ云音乐
+
+PC体验地址：https://snowcyans.gitee.io/cloudmusic/ -->
